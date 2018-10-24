a756bf958dbd77a41dd6ed7a2fc79c273b2ab653
viktor.terljuk <viktor.terljuk@gmail.com>
2018-10-24 10:30:39 +0300

    created test for Status.class

--- src/test/java/org/danit/luckyfit/entity/StatusTest.groovy
+++ src/test/java/org/danit/luckyfit/entity/StatusTest.groovy
@@ -1,4 +1,19 @@
 package org.danit.luckyfit.entity
 
+import org.junit.Test
+import static org.junit.Assert.assertEquals
+
 class StatusTest {
+
+    @Test
+    void testGetIdSetIdGetNameSetNameToString() {
+        def status = new Status()
+
+        status.setId(1)
+        status.setName("Test Name")
+
+        assertEquals(status.id, status.getId())
+        assertEquals(status.name, status.getName())
+        assertEquals("Status{id=1, name='Test Name'}", status.toString())
+    }
 }
